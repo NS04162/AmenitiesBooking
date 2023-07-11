2f6e6fa568c7371d6848803a576bfbc81e28f6fe
Prakash Rai <prakash.rai838@gmail.com>
2023-07-11 17:03:43 +0530

    RIC code changes

--- amenitiesbooking/src/main/java/com/citi/amenitiesbooking/mapper/RicAmenitiesViewMapper.java
+++ amenitiesbooking/src/main/java/com/citi/amenitiesbooking/mapper/RicAmenitiesViewMapper.java
@@ -3,13 +3,15 @@ package com.citi.amenitiesbooking.mapper;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface RicAmenitiesViewMapper {
 	
-	@Select("select distinct amenitiesName, AmenitiesCode from RicAmenitiesInfo")
+	@Select("select distinct amenitiesName, amenitiesCode from RIC_AmenitiesInfo")
+	@MapKey("amenitiesName")
 	List<Map<String, Object>> viewAmenities();
 
 }
